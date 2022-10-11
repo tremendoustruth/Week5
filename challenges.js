f52332db78e0d07ea057f34df85bd1da90002c69
Bushra Salama <bushra.salama@gmail.com>
2022-10-11 11:35:35 -0500

    morning exercises

--- challenges.js
+++ challenges.js
@@ -25,3 +25,64 @@ function outlierChecker(arr){
     }
 }
 outlierChecker(sample2);
+
+//Create a Phone Number
+
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+// Example:
+
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+// The returned format must be correct in order to complete this challenge.
+
+// Don’t forget the space after the closing parentheses!
+//             0 1 2 3 4 5 6 7 8 9
+let sample3 = [1,2,3,4,5,6,7,8,9,0];
+let sample4 = [9,8,7,6,5,4,3,2,1,0];
+
+function phoneCreater(arr){
+    let areacode = ""
+    for(i=0; i<3; i++){
+       areacode+= arr[i]
+    }
+    console.log(areacode)
+ 
+    let chunk1=""
+    for (i=3; i<6; i++){
+        chunk1+=arr[i]
+        
+    }
+    console.log(chunk1)
+
+    let chunk2=""  
+    for (i=6; i<arr.length; i++){
+        chunk2+=arr[i]
+    }
+    console.log(chunk2)
+
+    return "(" + areacode + ")" + " " + chunk1 + "-" + chunk2
+
+}
+console.log(phoneCreater(sample4))
+
+//additional solution:
+let sample5 = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+const createNumber = (arr) => {
+   let number = '' //iniitalizes number string
+   arr = arr.map(String); //converts each element in the array into a string using the .map method, which calls a function for each element in an array.
+   number += `(${arr[0]+arr[1]+arr[2]}) ` //area code chunk with space
+   number += `${arr[3] + arr[4] + arr[5]}-`//chunk1 with dash
+   number += arr[6] + arr[7] + arr[8] + arr[9] //chunk 2 (does not need $ because no dashes or spaces to be added and elements already strings)
+   return number
+}
+console.log(createNumber(sample5))
+
+//regex example - more study  
+const regex_phone = (arr) => {
+
+    let format = arr.join("").match(/(\d{3})(\d{3})(\d{4})/); //.match() for regex
+    console.log(format)
+    return `(${format[1]})-${format[2]}-${format[3]}`
+}
+
+console.log(regex_phone([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]));
