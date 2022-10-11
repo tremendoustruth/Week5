761d0437272657377c1bd3cadd913ad274232d64
Bushra Salama <bushra.salama@gmail.com>
2022-10-10 21:53:45 -0500

    employee file

---
+++ sql-basics/artist.sql
@@ -0,0 +1,17 @@
+//problem 1
+
+INSERT INTO artist (artist_id, name)
+VALUES (276, 'Heart of a Coward');
+INSERT INTO artist (artist_id, name)
+VALUES (277, 'Aurora');
+INSERT INTO artist (artist_id, name)
+VALUES (278, 'Nicki Minaj');
+
+//problem 2
+SELECT * FROM artist ORDER BY name DESC LIMIT 10;
+
+//problem 3
+SELECT * FROM artist ORDER BY name LIMIT 5;
+
+//problem 4
+SELECT * FROM artist WHERE name LIKE '%Black%';
---
+++ sql-basics/employee.sql
@@ -0,0 +1,18 @@
+//problem1
+SELECT last_name, first_name FROM employee WHERE city='Calgary';
+
+//problem 2
+SELECT MAX(birth_date) FROM employee;
+
+//problem 3
+SELECT MIN(birth_date) FROM employee;
+
+//problem 4
+SELECT * FROM employee WHERE last_name='Edwards' AND first_name='Nancy'; 
+SELECT * FROM employee WHERE reports_to=2;
+
+//problem 5
+SELECT COUNT(employee_id) FROM employee WHERE city='Calgary';
+
+
+
