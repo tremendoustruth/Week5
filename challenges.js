04c6dc64e41623500e6bb27438ebb5d8c31d038d
Bushra Salama <bushra.salama@gmail.com>
2022-10-13 15:44:38 -0500

    SpecTrial - data

--- challenges.js
+++ challenges.js
@@ -86,3 +86,28 @@ const regex_phone = (arr) => {
 }
 
 console.log(regex_phone([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]));
+
+// In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+// At the end of the first year there will be: 1000 + 1000 * 0.02 + 50 => 1070 inhabitants
+// At the end of the 2nd year there will be: 1070 + 1070 * 0.02 + 50 => 1141 inhabitants (** number of inhabitants is an integer **)
+// At the end of the 3rd year there will be: 1141 + 1141 * 0.02 + 50 => 1213
+// It will need 3 entire years.
+// More generally given parameters:
+// p0, percent, aug (inhabitants coming or leaving each year), p (population to surpass)
+// the function nbYear should return n number of entire years needed to get a population greater or equal to p.
+// aug is an integer, percent a positive or null floating number, p0 and p are positive integers (> 0)
+
+function nbYear(p0, growth, aug, p){ //where p0 is initial pop, growth is percentage pop growth yoy, aug, aug is the additional inidivuals arriving each year
+  ln(p - (years)*50) = ln(p0(1+aug)^n) 
+  ln(p - (years)*50) = n(p0(1+aug)) 
+  
+}
+
+//class review 
+
+let years = 0
+while (pop < popgoal){
+    population = population *(1+percent/100)+aug;
+    years++
+return years
+}
